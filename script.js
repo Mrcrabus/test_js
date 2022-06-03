c1f8b60b8f0d78585fe83cac9ad0bc8201278cc1
Igor <42350880+Mrcrabus@users.noreply.github.com>
2022-06-03 15:38:43 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let coursesList = [
+  {name: "Courses in England", prices: [0, 100]},
+  {name: "Courses in Germany", prices: [500, null]},
+  {name: "Courses in Italy", prices: [100, 200]},
+  {name: "Courses in Russia", prices: [null, 400]},
+  {name: "Courses in China", prices: [50, 250]},
+  {name: "Courses in USA", prices: [200, null]},
+  {name: "Courses in Kazakhstan", prices: [56, 324]},
+  {name: "Courses in France", prices: [null, null]},
+];
+
+const courses = document.querySelector(".courses")
+const from = document.querySelector(".from")
+const to = document.querySelector(".to")
+
+const button = document.querySelector(".button")
+
+coursesList.sort((a, b) => a.prices[0] - b.prices[0])
+
+console.log(to.value)
+
+const showCourses = (list) => {
+  courses.innerHTML = "";
+  
+  list.forEach((course, i) => {
+    courses.innerHTML += `
+        <li class="item">${ i + 1 }. ${ course.name } <b>${ course.prices[0] === null ? 0: course.prices[0] }
+                          ${ course.prices[1] === null ? '': course.prices[1] } </b>
+        </li>
+    `;
+  });
+}
+
+button.addEventListener("click", () => {
+  console.log(1)
+  let filteredList = [...coursesList]
+  filteredList = filteredList.filter(course => course.prices[0] <= from.value  && course.prices[1] <= (to.value || 1000000))
+  showCourses(filteredList);
+})
+
+showCourses(coursesList);
+
